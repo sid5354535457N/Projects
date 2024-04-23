3e9a5b7e05c9d7ebaf6b40ae7ff4eb67cc641e62
Siddhant Bisen <145115589+sid5354535457N@users.noreply.github.com>
2024-04-23 23:57:40 +0530

    Delete EmployManajement directory

--- EmployManajement/src/Query.java
+++
@@ -1,6 +0,0 @@
-public class Query {
-    static String insert="INSERT INTO empp (id,name,email,salary) VALUES (?,?,?,?)";
-    static String update="UPDATE empp SET name=? WHERE id=?";
-    static String delete="DELETE FROM empp WHERE id=?";
-    static String select="SELECT * FROM empp";
-}
